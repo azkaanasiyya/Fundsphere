86c7dc44beb5a3dea593682fec15672830782894
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-02-19 16:16:13 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Hero from './components/section/hero';
+import Hero from './components/section/Hero';
 import Ads from './components/section/ads';
 import Features from './components/section/features';
 import Plans from './components/section/plans';
