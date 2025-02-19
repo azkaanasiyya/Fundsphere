2435695ce83d6b267040567d5b1a84983de7271e
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-02-19 14:07:39 +0700

    feat(button): add pricing variant and merge pricing button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line react/prop-types
-const Button = ({ onClick, children }) => {
+const Button = ({ onClick, children, className, variant = "primary" }) => {
+  const baseStyles = "h-14 rounded-full px-8 py-2 font-medium transition";
+  const variants = {
+    primary: "bg-primary-500 text-white hover:bg-primary-400",
+    pricing: "w-full h-[56px] bg-primary-500 text-white hover:bg-primary-400",
+    default: "w-full h-[56px] border border-primary-500 text-primary-500 hover:bg-primary-100", 
+  };
+
   return (
-    <button
-      onClick={onClick}
-      className="h-[56px] rounded-full bg-primary-500 px-[32px] 
-                py-[8px] font-medium text-white 
-                transition hover:bg-primary-400"
-    >
+    <button onClick={onClick} className={`${baseStyles} ${variants[variant]} ${className}`}>
       {children}
     </button>
   );
