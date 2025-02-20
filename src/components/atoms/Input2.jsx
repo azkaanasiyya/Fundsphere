36627a2970623fafdeb9caa343055500244939ac
Azka Anasiyya Haris <azkaanasiyya@gmail.com>
2025-02-20 16:17:53 +0700

    style(input): update padding for email input

--- src/components/atoms/Input2.jsx
+++ src/components/atoms/Input2.jsx
@@ -5,7 +5,7 @@ export default function Input({ type = 'text', placeholder, className }) {
       type={type}
       placeholder={placeholder}
       className={`h-[57px] w-full rounded-full border border-neutral-100 
-                  bg-white px-4 py-[4px] pl-[32px] pr-[160px] 
+                  bg-white px-4 py-[4px] md:pl-[32px] pr-[160px] 
                   text-sm placeholder-neutral-300 focus:outline-none 
                   lg:h-[64px] lg:text-base ${className}`}
     />
